b3a28446e1fea391cd588d8449c5b10bdd10e533
Stephen Blum <blum.stephen@gmail.com>
2017-11-15 12:44:53 -0800

    removed auth-key bad practice pattern.

--- components/chat/index.js
+++ components/chat/index.js
@@ -14,16 +14,22 @@ export default class Chat extends Component {
     constructor(props) {
         super(props);
 
+        // Storage of Received Messages
+        this.state          = {};
+        this.state.messages = [];
+
+        // ChatEngine Init
         this.chatEngine = ChatEngineCore.create({
             publishKey   : ChatConfig.publishKey
         ,   subscribeKey : ChatConfig.subscribeKey
         });
 
+        // Connect yourself (triggers '$.ready' event)
         this.chatEngine.connect(
             this.props.userID   || 'stephenlb'
-        ,   this.props.userData || { name: 'StephenBlum' }
-        ,   this.props.userAuth || 'auth-key-3' );
+        ,   this.props.userData || { name: 'StephenBlum' } );
 
+        // When the Connection is Live
         this.chatEngine.on( '$.ready', (chat) => {
             console.log('Ready');
             this.me = chat.me;
@@ -44,6 +50,7 @@ export default class Chat extends Component {
     // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     receiveMessage(event) {
         console.log( 'Receiving:', JSON.stringify(event.data.text) );
+        this.state.messages.push(event);
         // ... TODO
     }
 
@@ -67,13 +74,16 @@ export default class Chat extends Component {
     // Render Chat UI
     // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     render() {
+                //<FlatList
+                //    data={[{key: 'a'}, {key: 'b'}]}
+                //    dataSource={this.state.messages}
+                //    renderItem={({item}) => <Text>{item.key}</Text>} />
         return (
             <View style={ChatStyle.view}>
                 <TextInput
                     style={ChatStyle.messageInput}
                     placeholder="Chat here"
-                    onSubmitEditing={ (event) => this.sendMessage(event) }
-                />
+                    onSubmitEditing={ (event) => this.sendMessage(event) } />
             </View>
         );
     }
